9f98586fe9d9b68c4d46394d25b10e7f0d3086ba
danieltassew <danieltassew25@gmail.com>
2026-01-28 16:32:02 +0300

    removed animated flip counter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/adapters.dart';
 import 'package:intl/intl.dart';
